05b970fcf45bb2968c46cd5121ac6e5f30938a33
piyawat-suayroop <equality.biz@gmail.com>
2024-09-19 00:03:17 +0700

    Add files via upload

---
+++ phpbasic/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP</title>
+</head>
+<body>
+    <?php
+    echo "Hello PHP";
+    echo "<br>";
+    echo "this is my first time to write PHP language";
+    define("MINSIZE",50);
+    echo "<br>";
+    echo "<br>";
+    echo MINSIZE;
+    ?>
+</body>
+</html>
---
+++ phpbasic/function26.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>writeMessage function</title>
+</head>
+<body>
+    <?php
+        function writeMessage() //create function
+        { //start function
+            echo "this is first function for write message";
+            echo "Have a nice program";
+        } //stop function
+
+        //call function
+        writeMessage();
+    ?>
+</body>
+</html>
